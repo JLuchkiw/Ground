6ad8359baa9a8e794b09f0d48c4e424ffe1d5844
Ellie Luchkiw <ellie.luchkiw@gmail.com>
2025-11-07 13:29:01 -0500

    edited prompt language for flow

--- web_app/src/data/GroundingPrompts.js
+++ web_app/src/data/GroundingPrompts.js
@@ -3,10 +3,10 @@ export const GroundingPrompts = [
         category: "see",
         title: "Name 5 things you can see:",
         prompts: [
-            "Can you find something around you that is a color you like?",
-            "Can you find another item that's the same color?",
+            "Can you find something around you that is a color you like? What color is it?",
+            "Can you find another item that's the same color? What is it?",
             "Can you spot something shiny or reflective?",
-            "Can you find something that looks textured?",
+            "What about something that looks textured?",
             "Can you find something small, that you would usually overlook?"
         ]
     },
@@ -14,8 +14,8 @@ export const GroundingPrompts = [
         category: "feel",
         title: "Name 4 things you can feel:",
         prompts: [
-            "Can you reach out and touch something soft or textured?",
-            "Can you find something warm or cool to the touch?",
+            "Can you reach out and touch something soft or textured? How does it feel?",
+            "Can you find something warm or cool to the touch? What is it?",
             "Touch your shirt with your fingertips and note as many descriptions as you can.",
             "Touch your fingertips to the palm of your other hand. Make note of what you feel together and separately in your fingers and palm."
         ]
@@ -24,8 +24,8 @@ export const GroundingPrompts = [
         category: "hear",
         title: "Name 3 things you can hear:",
         prompts: [
-            "Can you hear any distant sounds? Maybe something rhythmic or natural?",
-            "Can you hear any voices or animal noises?",
+            "Can you hear any distant sounds? Is it rhythmic or natural?",
+            "Can you hear any voices or animal noises? Who or what do you hear?",
             "How about a fan, ac, or breeze if you're outdoors?"
         ]
     },
@@ -33,7 +33,7 @@ export const GroundingPrompts = [
         category: "smell",
         title: "Name 2 things you can smell:",
         prompts: [
-            "Is there a scent nearby: coffee, air freshener, fabric, anything pleasant?",
+            "Is there a scent nearby: coffee, air freshener, fabric, anything pleasant? What is it?",
             "Can you find another smell that’s familiar or comforting?"
         ]
     },
@@ -41,7 +41,7 @@ export const GroundingPrompts = [
         category: "taste",
         title: "Name 1 thing you can taste:",
         prompts: [
-            "Can you take a sip of water, tea, coffee, or soda? Or notice the lingering taste in your mouth from a recent snack or drink?"
+            "Can you take a sip of water, tea, coffee, or soda? Or notice the lingering taste in your mouth from a recent snack or drink? What do you taste?"
         ]
     }
 ];
